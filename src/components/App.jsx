c2e97f0409d343ad9f963a95aa4f55ba67d09c16
jfrasinska <j.frasinska@gmail.com>
2023-12-03 19:49:23 +0100

    adding transcations component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Profile from './Profile';
 import Statistics from './Statistics';
 import FriendList from './FriendList';
+import TransactionHistory from './TransactionHistory';
 
 import user from '../user.json';
 import data from '../data.json';
 import friends from '../friends.json';
+import transactions from '../transactions.json';
 
 const App = () => {
   return (
@@ -20,6 +22,7 @@ const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
